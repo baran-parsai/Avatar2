cb0d4ddf2bd10b6708ed645374d2ac1d071820ee
Michael Jenkin <michaeljenkin@me.com>
2024-03-25 23:23:33 -0400

    Update AvatarAudioInput.cs

--- unity/Scripts/AvatarAudioInput.cs
+++ unity/Scripts/AvatarAudioInput.cs
@@ -6,22 +6,22 @@ using UnityEngine;
 using System;
 
 
-public class AvatarAudioInput : ROSBridgeSubscriber
+public class AvatarAudio : ROSBridgeSubscriber
 {
 
 	public new static string GetMessageTopic()
 	{
-		return "/avatar2/audio_input";
+		return "/avatar2/out_raw_audio";
 	}
 
 	public new static string GetMessageType()
 	{
-		return "avatar2_interfaces/msg/AudioInput";
+		return "avatar2_interfaces/msg/Audio";
 	}
 
 	public new static ROSBridgeMsg ParseMessage(JSONNode msg)
 	{
-		return new AudioInputMsg(msg);
+		return new AudioMsg(msg);
 	}
 
 	public static byte[] StringToByteArray(string hex)
@@ -50,24 +50,35 @@ public class AvatarAudioInput : ROSBridgeSubscriber
 
 	public new static void CallBack(ROSBridgeMsg msg)
 	{
-		Debug.Log("Render callback in /Avatar2/audio_input " + msg);
+		Debug.Log("Render callback for avatar audio " + msg);
 
-		AudioInputMsg x = (AudioInputMsg)msg;
+		AudioMsg x = (AudioMsg)msg;
 
 		byte[] wav = StringToByteArray(x.GetAudio());
 		Debug.Log("Assuming 16 bits signed audio file is " + (wav.Length - 44) / 2 + " samples long");
 
 
-
-		AudioClip clip = AudioClip.Create("speak", (wav.Length - 44) / 2, 1, 44100, false);
+		AudioClip clip = null;
+		if(x.GetFormat() == "WAV_1_44100")
+		    clip = AudioClip.Create("speak", (wav.Length - 44) / 2, 1, 44100, false);
+		else if(x.GetFormat() == "WAV_1_22050")
+			clip = AudioClip.Create("speak", (wav.Length - 44) / 2, 1, 22050, false);
+		else
+        {
+			Debug.Log("Unknown audio format " + x.GetFormat());
+			return;
+        }
 		clip.SetData(WavtoFloatArray(wav), 0);
 
-		GameObject obj = GameObject.Find("Sphere");
-
+        Debug.Log("Cueing up audio to play when we can");
+		GameObject obj = GameObject.Find("UMA avatar");
 		AudioSource ass = obj.GetComponent<AudioSource>();
 		ass.clip = clip;
+		Avatar avatar = obj.GetComponent<Avatar>();
+		avatar.waiting2play = true;
+		Debug.Log("All ready to play");
 
-		ass.Play();
+
 
 
 	}
