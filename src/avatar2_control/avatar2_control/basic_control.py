dd352ce5d304191d86c5a446c51e7e1cf886c91b
Michael Jenkin <jenkin@yorku.ca>
2024-07-08 00:01:51 -0400

    avatar2 control with publish

--- src/avatar2_control/avatar2_control/basic_control.py
+++ src/avatar2_control/avatar2_control/basic_control.py
@@ -22,6 +22,13 @@ def controller_create_root() -> py_trees.behaviour.Behaviour:
          qos_profile=py_trees_ros.utilities.qos_profile_unlatched(),
          topic_name="/avatar2/in_message", topic_type=TaggedString, blackboard_variables = {'in_message' : None})
 
+    # automatically publish interesting ros tops from the blackboard
+    out_message = py_trees_ros.publishers.FromBlackboard(name="out_mesage",
+        topic_name = '/avatar2/out_message',
+        topic_type = TaggedString,
+        qos_profile = py_trees_ros.utilities.qos_profile_unlatched(),
+        blackboard_variable = '/out_message')
+
     # Deal with meta commands to the avatar
     meta = MetaHandler.MetaHandler("meta handler")
 
@@ -40,6 +47,7 @@ def controller_create_root() -> py_trees.behaviour.Behaviour:
     topics2bb.add_child(in_message)
     root.add_child(topics2bb)
     root.add_child(bits)
+    root.add_child(out_message)
     root.add_child(idle)
 
     return root
@@ -50,6 +58,11 @@ def main(args=None):
     rclpy.init(args=args)
     root = controller_create_root()
     tree = py_trees_ros.trees.BehaviourTree(root = root, unicode_tree_debug = True)
+    provided = py_trees.blackboard.Client(name="Provided")
+    provided.register_key(key='avatar_name', access=py_trees.common.Access.WRITE)
+    provided.avatar_name = 'Mary'
+    print(provided)
+
     try:
         tree.setup(node_name = "basic_control", timeout = 15.0)
     except py_trees_ros.exceptions.TimedOutError as e:
